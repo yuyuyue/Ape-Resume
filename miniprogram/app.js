64a4b6eb52950941a6ea2ea4525cde1caa97522f
wzy <3101885298@qq.com>
2019-05-20 16:26:17 +0800

    页面授权

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -11,5 +11,33 @@ App({
     }
 
     this.globalData = {}
-  }
+    this.queryUsreInfo()
+  
+  },
+    queryUsreInfo() {
+      wx.cloud.callFunction({
+        // 云函数名称
+        name: 'user',
+        // 传给云函数的参数
+        data: {
+          opt: "selectById",
+          data: {}
+        },
+        success: function (res) {
+          // console.log(res.result);
+          if(!res.result.result.data.length){
+            wx.navigateTo({
+              url: '/pages/authorize/authorize'
+            })
+          }
+          getApp().globalData.userInfo = res.result;
+        },
+        fail:function (err) {
+          wx.navigateTo({
+            url: '/pages/authorize/authorize'
+          })
+        }
+      })
+    }
+
 })
