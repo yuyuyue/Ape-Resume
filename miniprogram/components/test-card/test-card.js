3484a6fa01284c9156601697cd15e00d5f945ff1
yby <1349807391@qq.com>
2019-05-29 15:02:14 +0800

    猿力值模块

---
+++ miniprogram/components/test-card/test-card.js
@@ -0,0 +1,151 @@
+// components/test-card/test-card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    cardData: Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    isSearch: false,
+    inputValue: '',
+    opt: '',
+    url: '',
+    toggle: false,
+    toggleImage: '../../images/down-arrow.png'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    search(event) {
+      if (!this.data.inputValue) {
+        return
+      }
+      this.setData({
+        isSearch: true,
+      })
+      switch (event.currentTarget.dataset.search) {
+        case 'github': {
+          this.setData({
+            url: `https://github.com/users/${this.data.inputValue}/contributions`,
+            opt: 'gitContributions'
+          })
+          break
+        }
+        case 'juejin': {
+          this.setData({
+            url: `https://juejin.im/search?query=${this.data.inputValue}&type=all`,
+            opt: 'juejin'
+          })
+          break
+        }
+        case 'leetcode': {
+          this.setData({
+            url: `https://leetcode-cn.com/${this.data.inputValue}/`,
+            opt: 'leetcode'
+          })
+          break
+        }
+        case 'leetcodeE': {
+          break
+        }
+        case 'niuke': {
+          break
+        }
+      }
+      var result = []
+      wx.cloud.callFunction({
+        name: 'search',
+        data: {
+          opt: this.data.opt,
+          url: this.data.url
+        },
+        success: (res) =>{
+          console.log(res.result.result)
+          result = res.result.result
+          console.log(result,'11111')
+          if (this.data.opt === 'gitContributions') {
+            wx.request({
+              url: `https://api.github.com/users/${this.data.inputValue}/repos`,
+              header: {
+                'content-type': 'application/json'
+              },
+              success: (res) => {
+                let watch = 0
+                let star = 0
+                let fork = 0
+                for (let key of res.data) {
+                  watch += key.watchers
+                  star += key.stargazers_count 
+                  fork += key.forks
+                }
+                console.log(watch, star, fork)
+                result.push(watch)
+                result.push(star)
+                result.push(fork)
+                console.log('------------',result)
+                let data = {
+                  name: this.properties.cardData.name,
+                  num: result
+                }
+                this.triggerEvent('searched', data)
+                this.toggle()
+              },
+              fail: (err) => {
+                console.log(err)
+              }
+            })
+          } else {
+            let data = {
+              name: this.properties.cardData.name,
+              num: result
+            }
+            this.triggerEvent('searched', data)
+            this.toggle()
+          }
+          
+        },
+        fail: (err) => {
+          console.log(err)
+        }
+      })
+      const time = new Promise((resovle, reject) => {
+        setTimeout(()=>{
+          resovle('ok')
+        }, 1000)
+      }) 
+      time.then(() => {
+        this.setData({
+          isSearch: false
+        })
+      })
+    },
+    getInput(event) {
+      const inputValue = encodeURI(event.detail.value)
+      this.setData({
+        inputValue
+      })
+    },
+    toggle() {
+      let toggle = !this.data.toggle
+      if (toggle) {
+        this.setData({
+          toggle,
+          toggleImage: '../../images/up-arrow.png'
+        })
+      } else {
+        this.setData({
+          toggle,
+          toggleImage: '../../images/down-arrow.png'
+        })
+      }
+      
+    }
+  }
+})
--- miniprogram/pages/test/test.js
+++ miniprogram/pages/test/test.js
@@ -5,57 +5,184 @@ Page({
    * 页面的初始数据
    */
   data: {
-
-  },
-  // 去绑定github
-  goGitHub(){
-    wx.navigateTo({
-      url: '../signGit/signGit',
-      success: (result)=>{
-        
+    cards: [
+      {
+        name: 'Github',
+        bgColor: 'bg-grey',
+        color: 'grey',
+        bottomColor: 'bottom-grey',
+        tip: '输入git昵称',
+        image: '../../images/github.svg',
+        search: 'github',
+        icon: [
+          {
+            name: '提交数',
+            iconPath: '../../images/submit.svg',
+            num: '',
+            color: 'grey'
+          },
+          {
+            name: '关注',
+            iconPath: '../../images/watch.svg',
+            num: '',
+            color: 'grey'
+          },
+         
+          {
+            name: '收藏',
+            iconPath: '../../images/star.svg',
+            num: '',
+            color: 'grey'
+          },
+          {
+            name: '叉子',
+            iconPath: '../../images/fork.svg',
+            num: '',
+            color: 'grey'
+          }
+        ]
       },
-      fail: ()=>{},
-      complete: ()=>{}
-    });
-  },
-  //去绑定leetcode
-  goLeetCode(){
-    wx.navigateTo({
-      url: '../signLc/signLc',
-      success: (result)=>{
-        
+      {
+        name: '掘金',
+        bgColor: 'bg-blue',
+        color: 'blue',
+        bottomColor: 'bottom-blue',
+        tip: '输入掘金昵称',
+        image: '../../images/juejin.svg',
+        search: 'juejin',
+        icon: [
+          {
+            name: '点赞',
+            iconPath: '../../images/good.svg',
+            num: '',
+            color: 'blue'
+          },
+          {
+            name: '文章被观看',
+            iconPath: '../../images/watch1.svg',
+            num: '',
+            color: 'blue'
+          },
+          {
+            name: '掘力值',
+            iconPath: '../../images/up.svg',
+            num: '',
+            color: 'blue'
+          },
+          {
+            name: '关注者',
+            iconPath: '../../images/fans.svg',
+            num: '',
+            color: 'blue'
+          }
+        ],
       },
-      fail: ()=>{},
-      complete: ()=>{}
-    });
+      {
+        name: 'Leetcode',
+        bgColor: 'bg-orange',
+        color: 'orange',
+        bottomColor: 'bottom-orange',
+        tip: '输入自定义域名',
+        image: '../../images/leetcode.svg',
+        search: 'leetcode',
+        icon: [
+          {
+            name: '竞赛数',
+            iconPath: '../../images/number.svg',
+            num: '',
+            color: 'orange'
+          },
+          {
+            name: '竞赛积分',
+            iconPath: '../../images/point.svg',
+            num: '',
+            color: 'orange'
+          },
+          {
+            name: '竞赛排名',
+            iconPath: '../../images/rank.svg',
+            num: '',
+            color: 'orange'
+          },
+          {
+            name: '刷题数',
+            iconPath: '../../images/problem.svg',
+            num: '',
+            color: 'orange'
+          },
+        ]
+      }
+    ]
   },
-  //去绑定掘金
-  goJuejin(){
-    wx.navigateTo({
-      url: '../signJuejin/signJuejin',
-      success: (result)=>{
-        
-      },
-      fail: ()=>{},
-      complete: ()=>{}
-    });
+  searched(event) {
+    const { name, num } = event.detail
+    const searchData = []
+    let cards = this.data.cards
+    for (let card of cards) {
+      const iconLength = card.icon.length
+      if (card.name === name) {
+        for (let i = 0; i < iconLength; i++) {
+          card.icon[i].num = num[i]
+        }
+      }
+      let data = []
+      for (let i = 0; i < iconLength; i++) {
+        data.push({
+          name: card.icon[i].name,
+          num:  card.icon[i].num
+        })
+      }
+      searchData.push({
+        name: card.name,
+        data: data
+      })
+    }
+    console.log('-------------',searchData)
+    this.setData({
+      cards
+    })
+    wx.setStorage({
+      key:"cards",
+      data: cards
+    })
+    wx.setStorage({
+      key: 'searchData',
+      data: searchData
+    })
+    wx.getStorage({
+      key: 'searchData',
+      success (res) {
+        console.log('====================',res.data)
+      }
+    })
   },
   // 返回首页
-  goIndex(){
+  goIndex() {
     wx.navigateTo({
       url: '../index/index',
-      success: (result)=>{
-        
+      success: (result) => {
+
       },
-      fail: ()=>{},
-      complete: ()=>{}
+      fail: () => { },
+      complete: () => { }
     });
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const self = this
+    wx.getStorage({
+      key: 'cards',
+      success(res) {
+        self.setData({
+          cards: res.data
+        })
+      },
+      fail(){
+        console.log('还没有存储Storage')
+      }
+    })
   },
 
   /**
